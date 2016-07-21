14dca829fcb2714446b4a7e533f7a0731409f913
Joel Lopez <jowie.jowie@gmail.com>
2016-07-21 16:36:34 +0200

    First commit

---
+++ mobile/src/main/java/io/pros/oassis_glass/MainActivity.java
@@ -0,0 +1,13 @@
+package io.pros.oassis_glass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
